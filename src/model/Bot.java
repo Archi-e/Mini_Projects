f48158d9df7bce805a76b4b48644dca1b44bbf36
Archie Tripathi <archie.webwork@gmail.com>
2024-04-17 03:02:58 +0530

    Adding Models, Enums

--- src/model/Bot.java
+++ src/model/Bot.java
@@ -1,6 +1,14 @@
 package model;
 
+import service.botPlayingStrategy.BotPlayingStrategy;
+
 public class Bot extends Player{
     private BotDifficultyLevel botDifficultyLevel;
-    private
+    private BotPlayingStrategy botPlayingStrategy;
+
+    public Bot(BotDifficultyLevel botDifficultyLevel) {
+        super("BOT", 'B', PlayerType.BOT);
+        this.botDifficultyLevel = botDifficultyLevel;
+
+    }
 }
